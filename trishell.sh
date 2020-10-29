b9d296d4d5a6bf4926d2cf8eb234ce06b12f45c6
Quentin <63068436+Sowtch@users.noreply.github.com>
2020-10-29 15:45:15 +0100

    Update trishell.sh

--- trishell.sh
+++ trishell.sh
@@ -3,40 +3,27 @@
 # on vérifie si il existe un parmatrè.
 test $# -ne 1 && echo "Need a command as parameter" && exit 1
 
-# Déclaration des variables
-nb_param="$#"       # nombre de paramètre
-cmd_rec=false       # Vrai si la commande -R est appelé, sinon faux.
-cmd_dec=false       # Vrai si la commande -d est appelé, sinon faux.
-cmd_nsmletpg=false  # Vrai si la commande -nsmletpg est appelé, sinon faux.
-isRep=false         # Vrai si on a bien un répertoire en paramètre, faux sinon.
-param_nsmletpg=""
+# Déclaration des variables...
+nb_param="$#"           # nombre de paramètre
+param_rec=false         # Vrai si la commande -R est appelé, sinon faux.
+param_dec=false         # Vrai si la commande -d est appelé, sinon faux.
+param_nsmletpg=false    # Vrai si la commande -nsmletpg est appelé, sinon faux.
+param_rep=false         # Vrai si on a bien un répertoire en paramètre, faux sinon.
 
-isDirectory() {
-    if test -d "$1"
-    then
-        isRep=true
-    fi
-}
-
+# Fonciton qui vérifie si le premier paramètre est bien une commande d'appel résursif (-R)
 isRecursif() {
-    if test "$1" == "-R"
-    then
-        cmd_rec=true
-    fi
+    test "$1" == "-R" && param_rec=true
 }
 
+# Fonciton qui vérifie si le premier paramètre est bien une commande pour trier de manière décroissante (-d)
 isDescending() {
-    if test "$1" == "-d"
-    then
-        cmd_dec=true
-    fi
+    test "$1" == "-d" && param_dec=true
 }
 
-isNsmletpg() {
-
+# Fonciton qui vérifie si le premier paramètre est bien une commande d'appel pour effecuter les tri (-nsmletpg)
+isDirectory() {
+    test -d "$1" && param_rep=true
 }
 
 isDescending $1
 echo $cmd_dec
-# On verifie si le paramètre 
-# on appell une fonction qui verifie les paramètres
