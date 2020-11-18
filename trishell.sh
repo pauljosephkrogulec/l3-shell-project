8e1d4ae1a592b41c5f46098ecd804e9f6e137d5c
Quentin <63068436+Sowtch@users.noreply.github.com>
2020-11-18 19:45:07 +0100

    Add function countFiles()
    - Implémentation d'une fonction qui compte les nombres de fichier d'une chaine de caractères passé en paramètre. (sera utile pour compter le nombre de fichier d'une sosu-chaine)
    - Suppression de la variable globale nbFiles (qui sert plus a rien)
    - Update des fonctions utilisant nbFiles en ajustant par rapport a la fonction countFiles()

--- trishell.sh
+++ trishell.sh
@@ -91,7 +91,7 @@ function checkCommands() {
     # Si un élément n'est pas valide, ou valide mais qu'un autre élément du même type à déjà été rencontré... alors la commande donnée en paramètre
     # est fausse et on sort du programme en indiquant l'option invalide.
 
-    local ind=1; local found
+    local ind=1;local found;local i
     for i in $ALL_PARAM
         do
         found=0
@@ -162,135 +162,50 @@ function createString() {
     # va stocker l'ensemble des fichiers en les séparant tous par le séparateur définit en constante ":"
     # Si la chaine a été crée on retourne 1, sinon 0
 
-    local ch=""
+    local chaine=""
     for i in "$1"/*
     do
         if test -f "$i"
-            then
-            ch="$ch$i$SEPARATOR"
+        then
+            chaine=$chaine"$i$SEPARATOR"
         elif test -d "$i" -a $param_rec -ne 0
             then
-            ch="$ch$(createString $i)"
+            chaine= createString $i
         fi
     done
-    ch="$ch"
-    echo $ch
-}
-function compareText(){
-    if test $1 \> $2 
-    then
-        echo 1
-    elif test $1 == $2 
-    then
-        echo 0
-    else
-        echo -1
-    fi
-}
-function compareNumber(){
-    if test $1 -gt $2 
-    then
-        echo 1
-    elif test $1 -eq $2 
-    then
-        echo 0
-    else
-        echo -1
-    fi
-}
-function printString() {
-    # Fonction qui va parcourir la chaine de caractère stringFiles contenant l'ensemble des fichiers du paramètres données.
-    # Et va afficher les fichiers séparés par un séparateur ":"
-
-        test $found -eq 0 && echo "invalid option -- '$i'" && exit 3
-        ind=`expr $ind + 1`
-    done
-    test $param_rep -eq 0 && echo "Need a directory as parameter" && exit 4
+    stringFiles=$stringFiles$chaine
 }
 
-function nameFile() {
-   # Fonction qui prend en paramètre un fichier et retourne son nom.
-    echo $(basename $1)
-}
+function countFiles() {
+    # Fonction qui va parcourir une chaine de caractère passé en paramètre contenant des fichiers.
+    # Et va retourner le nombre de ficheirs (séparé par le séparateur ":").
 
-function sizeFile() {
-    # Fonction qui prend en paramètre un fichier et retourne sa taille.
-    echo $(stat -c "%s" $1)
-}
-
-function lastDateFile() {
-    # Fonction qui prend en paramètre un fichier et retourne la date de sa dernière modification.
-    echo $(stat -c "%y" $1)
-}
-
-function linesFile() {
-   # Fonction qui prend en paramètre un fichier et retourne son nombre de lignes.
-   echo $(sed -n '$=' $1)
-}
-
-function extensionFile() {
-    # Fonction qui prend en paramètre un fichier et retourne son extension.
-    echo $(sed 's/^.*\(...$\)/\1/' <<< $1)
-}
-
-function ownerFile() {
-    # Fonction qui prend en paramètre un fichier et retourne le propriétaire.
-    echo $(stat -c "%U" $1)
-}
-
-function groupFile() {
-    # Fonction qui prend en paramètre un fichier et retourne le groupe du propriétaire.
-    echo $(stat -c "%G" $1)
-}
-
-function createString() {
-    # Fonction qui à partir de du répertoire donné en paramètre de la commande,
-    # va stocker l'ensemble des fichiers en les séparant tous par le séparateur définit en constante ":"
-    # Si la chaine a été crée on retourne 1, sinon 0
-
-    local ch=""
-    for i in "$1"/*
-    do
-        if test -f "$i"
-            then
-            ch="$ch$i$SEPARATOR"
-        elif test -d "$i" -a $param_rec -ne 0
-            then
-            ch="$ch$(createString $i)"
-    local len=`expr length $stringFiles`; local ch=""
-    local carac=""
+    local len=`expr length $1`;local ch=""
+    local carac="";local res=0
     for i in `seq 0 $len`
         do
-        carac=${stringFiles:i:1}
-        if test "$carac" == $SEPARATOR
+        carac=${1:i:1}
+        if test "$carac" == "$SEPARATOR"
             then
-            echo "$ch"
+            res=`expr $res + 1`
             ch=""
         else
             ch=$ch"$carac"
         fi
     done
-    ch="$ch"
-    echo $ch
+    echo "$res"
 }
 
 function printString() {
     # Fonction qui va parcourir la chaine de caractère stringFiles contenant l'ensemble des fichiers du paramètres données.
     # Et va afficher les fichiers séparés par un séparateur ":"
 
-    local len=`expr length $stringFiles`; local ch=""
-    local carac=""
+    local i=1;local nbFiles=$(countFiles $1)
 
-    for i in `seq 0 $len`
+    while test "$i" -le "$nbFiles"
         do
-        carac=${stringFiles:i:1}
-        if test "$carac" == $SEPARATOR
-            then
-            echo "$ch"
-            ch=""
-        else
-            ch=$ch"$carac"
-        fi
+        echo $(echo $1 | cut -d':' -f"$i")
+        i=`expr $i + 1`
     done
 }
 
@@ -318,31 +233,62 @@ function compareNumber() {
 }
 
 function tri_d() {
-    # Fonction qui prend en paramètre une chaine de caractère contenant les fichiers,
-    # et va trier cette chaine de manière décroissante.
+    # Fonction qui prend en paramètre récupère chaque fichier de la chaine de caractère à l'aide de la commande cut,
+    # et va ajouter chaque fichier dans une nouvelle chaine qui remplacera la chaine de fichier actuelle..
 
+    local i=$nbFiles
+    local newString="";local word=""
+
+    while test "$i" -ne 0
+        do
+        word=$(echo $stringFiles | cut -d':' -f"$i")
+        newString=$newString"$word"$SEPARATOR
+        i=`expr $i - 1`
+    done
+    stringFiles="$newString"
 }
+
+function tri_n() {
+    # Fonction qui prend en paramètre récupère chaque fichier de la chaine de caractère à l'aide de la commande cut,
+    # et va ajouter chaque fichier dans une nouvelle chaine qui remplacera la chaine de fichier actuelle..
+
+    local i=1;local j=1
+    local newString="";local file="";local file2="";local numberF=0;local name1="";local name2=""
+
+    while test "$i" -le "$nbFiles"
+        do
+        file=$(echo $stringFiles | cut -d':' -f"$i")
+
+        if test $numberF -eq 0
+            then
+            newString="$file"
+        else
+            j=0;found=0
+            name1=$(basename $file)
+            while test "$j" -le "$numberF"
+                do
+                file2=$(echo $newString | cut -d':' -f"$i")
+                name2=$(basename $file2)
+
+                local cmp=$(compareTest $name1 $name2)                
+            done
+        fi
+        
+
+        newString=$newString"$word"$SEPARATOR
+        i=`expr $i + 1`
+    done
+    stringFiles="$newString"
+}
+
 
 function main() {
     # Fonction qui nb prend rien en paramètre et exécute le programme.
 
     # On vérifie la commande donnée.
     checkCommands
-    stringFiles=$(createString $save_rep)
-    echo $stringFiles
-    printString
-}
-
-# on appelle la fonction main pour lancer le programme
-main
-
-function main() {
-    # Fonction qui nb prend rien en paramètre et exécute le programme.
-
-    # On vérifie la commande donnée.
-    checkCommands
-    stringFiles=$(createString $save_rep)
-    printString
+    createString $save_rep
+    printString $stringFiles
 }
 
 # on appelle la fonction main pour lancer le programme
